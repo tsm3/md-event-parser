9e8f1d93bacbda8478ef728fd13da91d50e9be4a
tsm3 <tristan.slayde.mansfield@gmail.com>
2023-10-24 14:41:48 -0500

    Added with_date constructor for EventModel

--- src/model.rs
+++ src/model.rs
@@ -21,27 +21,49 @@ type Result<T> = std::result::Result<T, EventParseError>;
 pub struct EventModel {
   date: NaiveDate, // Make this just a datetime, mandatory
   time: Option<NaiveTime>, // If None, all day
-  place: Option<String>, // Should this be mandatory?
+  place: String, // Should this be mandatory? yuh just empty string if None
   title: String, // This is mandatory, but just a String
 }
 
 impl EventModel {
+  const DATEFMT: &str = "%d %b %Y";
+
   pub fn new(
     date: String,
     time: Option<String>,
     place: Option<String>,
     title: String
   ) -> Result<EventModel> {
-    let date_struct = match NaiveDate::parse_from_str(&date, "%d %b %Y") {
+    let date_struct = match NaiveDate::parse_from_str(&date, EventModel::DATEFMT) {
       Ok(d) => d,
       Err(_) => return Err(EventParseError::default()),
     };
 
-    // let t_struct = match 
+    /* time is hard bc I don't want to always do hour:minute I wanna leave it as like, 6 PM instead of 6:00 PM */
+
+    let p = match place {
+      Some(s) => s,
+      None => "".to_owned(),
+    };
       
-    Ok(EventModel { date: date_struct, title, ..Default::default()})
+    Ok(EventModel { date: date_struct, title: title, place: p, ..Default::default()})
   }
+
+  pub fn with_date(date_str: String) -> Result<EventModel> {
+    let date_struct = match NaiveDate::parse_from_str(&date_str, EventModel::DATEFMT) {
+      Ok(d) => d,
+      Err(_) => return Err(EventParseError::default()),
+    };
+    Ok(EventModel{
+      date:date_struct,
+      ..Default::default()
+    })
+
+  }
+
 }
+
+
 
 
 #[cfg(test)]
@@ -66,6 +88,13 @@ mod tests {
         None, 
         "Test Title".to_string());
       dbg!(em);
+    }
+
+    #[test]
+    fn test_with_date() {
+      let em = EventModel::with_date("15 Feb 2023".to_owned());
+      // dbg!(em);
+      assert!(em.is_ok());
     }
     
     #[test]
